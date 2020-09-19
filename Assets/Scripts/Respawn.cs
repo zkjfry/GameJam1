2e25cf35bf9a572fe1055c6839d968f53cf2172f
zkjfry <jeffreychen0826@gmail.com>
2020-09-20 02:57:25 +0800

    1

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+
+	public float boundary;
+
+	void FixedUpdate() {
+		if (transform.position.y < boundary) {
+			transform.position = new Vector3(0, 5, 0);
+		}
+	}
+
+}
